a536c5f401f7dcc49e75079a8357b9c04eaa39f8
Shah-Alam Hossain <47415967+SA940@users.noreply.github.com>
2019-02-07 16:53:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prograssive Pre-cadete High School</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv ="X-UA-compatible" content="IE-edge">
+	
+	
+	<link rel="stylesheet" href="style.css"> 
+		
+		
+	
+</head>
+<body>
+
+		<div class="header"> 
+			<div> 
+			<a class="home" href=""><i class="fa fa-home fa-fw"></i>Home</a>
+			</div>
+			<nav class="menu">
+				<ul>
+					<li ><a href=""><i class="fa fa-user-plus"></i> SignUp</a></li>
+					<li ><a href=""><i class="fa fa-sign-in"></i> Signin</a></li>
+					<li ><a href=""><i class="fa fa-phone-square"></i> Contact us</a></li> 
+				    <li ><a href=""><i class="fa fa-envelope-o"></i> Email</a></li>
+					
+					</ul>
+					
+				</nav>
+		
+		</div>
+		
+		<div class="coitainer"> 
+
+				<div class="logo"> 
+					<img class="image"src="image/ppchs.png" "alt="ppchs">
+				</div>
+
+				<div class="Srch">
+						<input class="srch-bar" type="text" placeholder="Search" name="srch">
+						<button class="srch-btn" type="submit" name="src-bnt">Search</button>
+				</div>
+				
+				<div class="navbar">
+						
+					<ul> 								
+						<li><a href=""><i class="fa fa-university fa-1x"></i> About Us</a>
+	
+							<ul>
+								<li><a href="#">Link 1</a></li>
+								<li><a href="#">Link 2</a></li>
+								<li><a href="#">Link 3</a></li>
+								<li><a href="#">Link 3</a></li>
+								<li><a href="#">Link 3</a></li>
+								<li><a href="#">Link 3</a></li>
+								<li><a href="#">Link 3</a></li>
+							</ul>
+
+						</li>
+						
+						<li><a href=""><i class="fa fa-pencil-square-o"></i> Admission</a> </li>
+						<li><a href=""><i class="fa fa-book fa-1x"></i> Academic</a> </li>
+						<li><a href=""><i class="fa fa-fw-square fa-key fa-1x"></i> Career</a> </li>
+						<li><a href=""><i class="fa fa-fw hub text-inverse fa-asterisk fa-1x"></i> IQAC</a> </li>
+
+					</ul>
+				</div>
+			<div class="slideshow">
+			  <div class="mySlides">
+					<img id="sldimg" src="image/pic1.jpg" >
+					<div class="lable">
+					  
+					 <p><b>Education for tomorrow's world, for a better world.</b></p> 
+					</div>
+				</div>
+				<div class="mySlides">
+				<img id="sldimg" src="image/pic2.jpg" >
+					<div class="lable">
+					  <p><b>Bring out your best for next generations.</b></p> 
+					  
+					</div>
+				 </div>
+				
+				<div class="mySlides">
+					<img id="sldimg" src="image/pic3.jpg" >
+					<div class="lable">
+					  <p style="color:green"><b>Make a green world.</b></p> 
+					</div>
+				</div>
+				</div>
+				<div class="colm"> 
+				<div class="n1"> 
+				<ul>
+				<li>
+					
+					
+						<h4>Admission</h4>
+						
+						<img class ="adm"src="image/pic3.jpg"  >
+						<div class="news_lbl">
+						<a href="#"><i class="fa fa-university fa-1x"></i>Admission Open</a>
+						 </div>
+						
+						
+					
+					
+					</li>
+					<li><div class="n2"> 
+					
+						<h4>New</h4>
+						
+						<img class ="adm"src="image/pic3.jpg"  >
+						<div class="news_lbl">
+						<a href="#"><i class="fa fa-university fa-1x"></i>Read News</a>
+						 </div>
+						
+						
+					
+					</div> 
+					</li>
+					<li><div class="n2"> 
+					
+						<h4>Founder's Message</h4>
+						
+						<img class ="adm"src="image/pic3.jpg"  >
+						<div class="news_lbl">
+						<a href="#"><i class="fa fa-university fa-1x"></i>Read Message</a>
+						 </div>
+						
+						
+					
+					</div> 
+					</li>
+					</ul>
+					</div> 
+				</div>
+				
+			
+
+		</div>
+		
+		
+		
+		<div class="footer">
+		
+		<p>Copyright &copy;2019 Prograssive Pre-cadete High School || Allright Reserved </p>
+		<p>Designed & Developed By : Shah-Alam Hossain (Web & Software Developer )</p>
+		
+
+						
+					<div class="cont">
+					<h6>Contact with us:</h6>
+							<ul> 
+							
+								 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								 <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								 <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								  <li><a href="#"><i class="fa fa-youtube-square"></i></a></li>
+							
+							
+							</ul>
+					</div>
+			 
+		</div>
+		
+		
+		<script src="slidescript.js"></script>
+		
+
+	
+</body>
+</html>
