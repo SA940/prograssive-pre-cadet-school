a0d62b3d7277855107289df9a8413c16173d5f59
Shah-Alam Hossain <47415967+SA940@users.noreply.github.com>
2019-02-07 16:39:33 +0600

    image

---
+++ home.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Game for life</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+	<!--header --->
+	
+		<div class="container-fluid top-bar">
+		<div class="conainer">
+		
+			<div class="row mrow"> 
+				
+					<ul> 
+						<li><a href="#">POPULAR GENERS : </a></li>
+						<li><a href="#">ACTION</a></li>
+						<li><a href="#">ADULT</a></li>
+						<li><a href="#">ANIME</a></li>
+						<li><a href="#">RACING</a></li>
+						<li><a href="#">RPG</a></li>
+						<li><a href="#">SIMULATION</a></li>
+						<li><a href="#">SPORT</a></li>
+						<li><a href="#">STRATEGY</a></li>
+						<li><a href="#">SURVIVAL</a></li>
+						<li><a href="#">VR</a></li>
+						
+					</ul>
+				
+			</div><!-- end of row -->
+		
+		</div> <!--- container end --->
+		
+		</div><!-- container-fluid end -->
+	
+	
+		
+	
+	<div class="text-container"> 
+	
+	<h1><strong>GAME FOR LIFE</strong></h1>
+	
+	</div><!-- text-container end -->
+	
+	<div class="conainer mybody"> 
+			<div class="row "> 
+			
+				<div class="col-sm-22 my_menu">
+				
+			
+			
+					<ul class="nav navabr-nav pull-right nv">
+					
+						<li class="active"style="text-shadow:2px 0px 8px cyan"><a href="#"><i class="fa fa-home"></i> HOME</a></li>
+						<li><a href="#"><i class="fa fa-tag" area-hidden="true;"></i> GENER</a>
+						<ul class="dropdown">
+							<li><a href="#">ACTION</a></li>
+							<li><a href="#">ADVENTURE</a></li>
+							<li><a href="#">ADULT</a></li>
+							<li><a href="#">ANIME</a></li>
+							<li><a href="#">BULILDING</a></li>
+							<li><a href="#">CASUAL</a></li>
+							<li><a href="#">HIDDEN / OBJECT</a></li>
+							<li><a href="#">HORROR</a></li>
+							<li><a href="#">MATURE</a></li>
+							<li><a href="#">NUDITY</a></li>
+							<li><a href="#">PUZZLE</a></li>
+							<li><a href="#">RACING</a></li>
+							<li><a href="#">RPG</a></li>
+							<li><a href="#">RTS</a></li>
+							<li><a href="#">SCI-FI</a></li>
+							<li><a href="#">SHOOTER</a></li>
+							<li><a href="#">SIMULATION</a></li>
+							<li><a href="#">SPORT</a></li>
+							<li><a href="#">STRATEGY</a></li>
+							<li><a href="#">SURVIVAL</a></li>
+							<li><a href="#">TEXT-BASED</a></li>
+							<li><a href="#">VR GAMES</a></li>
+						</ul>
+						
+						
+						</li>
+						<li><a href="#"><i class="fa fa-sliders"></i> VR GAMES</a></li>
+						<li><a href="#"><i class="fa fa-random"></i> RANDOM</a></li>
+						<li><a href="#"><i class="fa fa-info-circle"></i> FAQ</a></li>
+						<li><a href="#"><i class="fa fa-list"></i> GAME LIST</a></li>
+						<li><a href="#"><i class="fa fa-question-circle"></i> GAME REQUEST</a></li>
+					</ul>
+				
+				</div><!--  end of col -->
+			
+			</div><!-- end of row -->
+			
+			<div class="slideshow">
+			  <div class="mySlides">
+					<img id="sldimg" src="image/slideimage/borderlands_2.png" >
+					<div class="lable">
+					</div>
+				</div>
+				<div class="mySlides">
+				<img id="sldimg" src="image/slideimage/call_of_duty.png" >
+					<div class="lable">
+					 
+					</div>
+				 </div>
+				
+				<div class="mySlides">
+					<img id="sldimg" src="image/slideimage/sniper_elit.png" >
+					<div class="lable">
+					</div>
+				</div>		
+				<div class="mySlides">
+					<img id="sldimg" src="image/slideimage/farcry5.jpg" >
+					<div class="lable">
+					
+					</div>
+				</div>
+				<div class="mySlides">
+					<img id="sldimg" src="image/slideimage/Crysis_3.png" >
+					<div class="lable">
+					</div>
+					 
+				</div>
+				</div><!-- slides end -->
+				
+				<br>
+			
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		<p>sjdkjsdpspkdfdnvidfeen niacymoa;e,fkdfcae</p>
+		</div><!-- end of container -->
+	<!--- header end -->
+	
+	<!-- bootstrap --->
+	<script type="text/javascript" src="bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+	<script type="text/javascript" src="jquiry.js"></script>
+	<script src="slidescript.js"></script>
+</body>
+</html>
